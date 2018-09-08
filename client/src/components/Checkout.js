16502f7cc03940fe5a9d09353814eb91c6201710
Biboswan Roy <biboswan98@gmail.com>
2018-09-08 13:53:36 +0530

    Substitute collpaible with  expandpanel of material-ui

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,12 +1,17 @@
 import React, { Fragment, PureComponent } from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import PhoneVerification from './PhoneVerification';
 import Payment from './Payment';
 import ContactDetailsForm from './ContactDetailsForm/ContactDetailsForm';
-import { Collapsible, CollapsibleItem, Row, Col } from 'react-materialize';
+import { Row, Col, Icon } from 'react-materialize';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { connect } from 'react-redux';
 
-export default class Checkout extends PureComponent {
+class Checkout extends PureComponent {
   render() {
     return (
       <Fragment>
@@ -14,17 +19,38 @@ export default class Checkout extends PureComponent {
         <main>
           <Row>
             <Col s={12} l={8}>
-              <Collapsible accordion popout defaultActiveKey={0}>
-                <CollapsibleItem header="CONTACT DETAILS" icon="filter_drama">
-                  <ContactDetailsForm />
-                </CollapsibleItem>
-                <CollapsibleItem header="PHONE VERIFICATION" icon="place">
+              <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Icon>phone</Icon>
+                  PHONE VERIFICATION
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
                   <PhoneVerification />
-                </CollapsibleItem>
-                <CollapsibleItem header="PAYMENT" icon="place">
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+              <ExpansionPanel disabled={this.props.checkout.step1 === 'undone'}>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Icon>place</Icon>
+                  DELIVERY ADDRESS
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <ContactDetailsForm prop />
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+              <ExpansionPanel
+                disabled={
+                  this.props.checkout.step1 === 'undone' ||
+                  this.props.checkout.step2 === 'undone'
+                }
+              >
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Icon>payment</Icon>
+                  PAYMENT
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
                   <Payment />
-                </CollapsibleItem>
-              </Collapsible>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
             </Col>
           </Row>
         </main>
@@ -33,3 +59,12 @@ export default class Checkout extends PureComponent {
     );
   }
 }
+
+function mapStateToProps({ checkout }) {
+  return { checkout };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(Checkout);
