818d0f032a8a30ae417f0f7555b22ca70a9f230a
Biboswan Roy <biboswan98@gmail.com>
2018-09-08 13:05:32 +0530

    Empty unknown cart on login

--- client/src/reducers/unknownCartReducer.js
+++ client/src/reducers/unknownCartReducer.js
@@ -1,10 +1,10 @@
-import { UPDATE_CART_UNKNOWN, DELETE_CART_UNKNOWN } from '../actions/types';
+import { UPDATE_CART_UNKNOWN, UPDATE_CART_KNOWN } from '../actions/types';
 
 export default function(state = { items: {}, count: 0 }, action) {
   switch (action.type) {
     case UPDATE_CART_UNKNOWN:
       return { ...action.payload };
-    case DELETE_CART_UNKNOWN:
+    case UPDATE_CART_KNOWN:
       return { items: {}, count: 0 };
     default:
       return state;
