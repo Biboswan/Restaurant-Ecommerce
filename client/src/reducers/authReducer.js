0434dd597cb79f017ca2b2c55df57e1ff0d4d347
Biboswan Roy <biboswan98@gmail.com>
2018-09-20 17:36:39 +0530

    Initilize cart after succesful order submit

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_USER, UPDATE_CART_KNOWN } from '../actions/types';
+import { FETCH_USER, UPDATE_CART_KNOWN, SUBMIT_ORDER } from '../actions/types';
 
 export default function(state = null, action) {
   switch (action.type) {
@@ -6,6 +6,8 @@ export default function(state = null, action) {
       return action.payload || false;
     case UPDATE_CART_KNOWN:
       return { ...action.payload };
+    case SUBMIT_ORDER:
+      return { ...state, cart: { count: 0, items: {} } };
     default:
       return state;
   }
